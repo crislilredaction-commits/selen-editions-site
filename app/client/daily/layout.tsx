75741b108893dfbb88146e522429b408460a591b
Lil <crislil.redaction@gmail.com>
2026-08-10 06:37:06 +0200

    add Daily learners navigation

--- app/client/daily/layout.tsx
+++ app/client/daily/layout.tsx
@@ -17,6 +17,7 @@ export default function DailyClientLayout({ children }: { children: React.ReactN
       >
         <Link href="/client/daily/formations" style={linkStyle}>Formations</Link>
         <Link href="/client/daily/sessions" style={linkStyle}>Sessions</Link>
+        <Link href="/client/daily/apprenants" style={linkStyle}>Apprenants</Link>
         <Link href="/client/daily/dossiers" style={linkStyle}>Dossiers de session</Link>
         <Link href="/client/daily" style={linkStyle}>Suivi & documents</Link>
         <Link href="/client/daily/organisation" style={linkStyle}>Mon organisme</Link>
