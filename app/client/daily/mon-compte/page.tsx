b5e8c72e2ee2953b6cdc0c8f2b83101625b9fe46
Lil <crislil.redaction@gmail.com>
2026-08-28 04:11:32 +0200

    feat(daily): relier préférences de rappels au profil

--- app/client/daily/mon-compte/page.tsx
+++ app/client/daily/mon-compte/page.tsx
@@ -1,5 +1,6 @@
 import DailyAccountPanel from "@/components/daily/DailyAccountPanel";
+import DailyReminderPreferences from "@/components/daily/DailyReminderPreferences";
 
 export default function DailyAccountPage() {
-  return <DailyAccountPanel />;
+  return <><DailyAccountPanel/><DailyReminderPreferences/></>;
 }
